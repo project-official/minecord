1a6e6219c1a5106fc84dbf2d6d6bdcaddc126583
gunyu1019 <gunyu1019@gmail.com>
2021-06-05 03:43:34 +0900

    Add Custom, Markdown support and color chat support

--- src/main/kotlin/xyz/netherald/wild/discord/listeners/DiscordListener.kt
+++ src/main/kotlin/xyz/netherald/wild/discord/listeners/DiscordListener.kt
@@ -8,6 +8,7 @@ import net.dv8tion.jda.api.events.GenericEvent
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.EventListener
 import net.kyori.adventure.text.TextComponent
+import net.kyori.adventure.text.TranslatableComponent
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.event.EventHandler
@@ -20,6 +21,139 @@ import xyz.netherald.wild.discord.WildDiscord
 
 @Suppress("DEPRECATION")
 class DiscordListener(private val plugin: WildDiscord) : EventListener, Listener {
+    val listColor: List<String> = listOf("<black>", "<dark_blue>", "<dark_green>", "<dark_aqua>", "<red>",
+        "<dark_purple>", "<gold>", "<gray>", "<dark_gray>", "<blue>", "<green>", "<aqua>", "<purple>", "<yellow>",
+        "<white>")
+
+    private fun replaceChatColor(message: String): String {
+        return message.replace("<black>", "${ChatColor.BLACK}")
+            .replace("<dark_blue>", "${ChatColor.DARK_BLUE}")
+            .replace("<dark_green>", "${ChatColor.DARK_GREEN}")
+            .replace("<dark_aqua>", "${ChatColor.DARK_AQUA}")
+            .replace("<red>", "${ChatColor.DARK_RED}")
+            .replace("<dark_purple>", "${ChatColor.DARK_PURPLE}")
+            .replace("<gold>", "${ChatColor.GOLD}")
+            .replace("<gray>", "${ChatColor.GRAY}")
+            .replace("<dark_gray>", "${ChatColor.DARK_GRAY}")
+            .replace("<blue>", "${ChatColor.BLUE}")
+            .replace("<green>", "${ChatColor.GREEN}")
+            .replace("<aqua>", "${ChatColor.AQUA}")
+            .replace("<purple>", "${ChatColor.LIGHT_PURPLE}")
+            .replace("<yellow>", "${ChatColor.YELLOW}")
+            .replace("<white>", "${ChatColor.WHITE}")
+            .replace("<magic>", "${ChatColor.MAGIC}")
+            .replace("<bold>", "${ChatColor.BOLD}")
+            .replace("<strikethrough>", "${ChatColor.STRIKETHROUGH}")
+            .replace("<underline>", "${ChatColor.UNDERLINE}")
+            .replace("<italic>", "${ChatColor.ITALIC}")
+            .replace("<reset>", "${ChatColor.RESET}")
+    }
+
+    private fun replaceChatFormat(event: MessageReceivedEvent, formatter: String, Markdown: Boolean, CustomColor: Boolean): String {
+        val msg: String;
+
+        val s: String = replaceChatColor(formatter);
+        var m_message: String = "";
+
+        var stringMessage: String = event.message.contentRaw
+
+        if (Markdown) {
+            var setBold: Boolean = false
+            var setColor: String? = null
+            var setItalic: Boolean = false
+            var setUnderline: Boolean = false
+            var setStrikethrough: Boolean = false
+
+            var index: Int = 0
+            while (index <= stringMessage.length -1) {
+                var value: String = stringMessage.slice(index..stringMessage.length -1);
+                var alreadyColor: Boolean = false
+
+                if (CustomColor) {
+                    for (color in listColor) {
+                        if (value.startsWith(color)) {
+                            setColor = color
+                            index += color.length
+                            alreadyColor = true
+                            break
+                        }
+                    }
+                }
+
+                if (value.startsWith("**")) {
+                    if (setBold) {
+                        setBold = false
+                        m_message += "${ChatColor.RESET}"
+                        if (CustomColor && setColor != null) {
+                            m_message += setColor
+                            index += setColor.length
+                        }
+                    } else {
+                        setBold = true
+                        m_message += "${ChatColor.BOLD}"
+                    }
+                    index += 2
+                } else if (value.startsWith("*")) {
+                    if (setItalic) {
+                        setItalic = false
+                        m_message += "${ChatColor.RESET}"
+                        if (CustomColor && setColor != null) {
+                            m_message += setColor
+                            index += setColor.length
+                        }
+                    } else {
+                        setItalic = true
+                        m_message += "${ChatColor.ITALIC}"
+                    }
+                    index += 1
+                } else if (value.startsWith("~~")) {
+                    if (setStrikethrough) {
+                        setStrikethrough = false
+                        m_message += "${ChatColor.RESET}"
+                        if (CustomColor && setColor != null) {
+                            m_message += setColor
+                            index += setColor.length
+                        }
+                    } else {
+                        setStrikethrough = true
+                        m_message += "${ChatColor.STRIKETHROUGH}"
+                    }
+                    index += 2
+                } else if (value.startsWith("__")) {
+                    if (setUnderline) {
+                        setUnderline = false
+                        m_message += "${ChatColor.RESET}"
+                        if (CustomColor && setColor != null) {
+                            m_message += setColor
+                            index += setColor.length
+                        }
+                    } else {
+                        setUnderline = true
+                        m_message += "${ChatColor.UNDERLINE}"
+                    }
+                    index += 2
+                } else {
+                    if (!alreadyColor) {
+                        index += 1
+                        m_message += value[0]
+                    }
+                }
+            }
+        } else {
+            m_message = stringMessage
+        }
+
+        if (CustomColor) {
+            msg = replaceChatColor(m_message)
+        }
+        else {
+            msg = m_message
+        }
+
+        return s.replace("<message>", msg)
+            .replace("<sender>", event.author.asTag)
+            .replace("<mention>", event.author.asMention)
+    }
 
     override fun onEvent(event: GenericEvent) {
         if(event is MessageReceivedEvent) {
@@ -27,6 +161,7 @@ class DiscordListener(private val plugin: WildDiscord) : EventListener, Listener
                 if (event.channelType != ChannelType.PRIVATE) {
                     if(event.channel.id == WildDiscord.instance?.config?.getString("channelId"))
                         if (event.message.contentRaw == "!online") {
+                            val noMember: String? = plugin.config.getString("NoMemberMessage")
                             if (plugin.config.getInt("styleOnlineCommand") == 0) {
                                 var memberStr = "**온라인 유저** : \n```"
                                 memberStr += "인원: ${Bukkit.getOnlinePlayers().size}명/ ${Bukkit.getMaxPlayers()}명\n"
@@ -35,7 +170,7 @@ class DiscordListener(private val plugin: WildDiscord) : EventListener, Listener
                                         memberStr += "$i: ${player.name}\n"
                                     }
                                 } else {
-                                    memberStr += "사람이 없습니다\n"
+                                    memberStr += "${noMember?:"사람이 없습니다."}\n"
                                 }
                                 memberStr += "```"
                                 memberStr += "========${WildDiscord.serverAddress ?: ""}========\n"
@@ -43,28 +178,36 @@ class DiscordListener(private val plugin: WildDiscord) : EventListener, Listener
                                 event.channel.sendMessage(memberStr).queue()
                             }
                             else if (plugin.config.getInt("styleOnlineCommand") == 1) {
-                                var embed = EmbedBuilder().setTitle("**온라인 유저**")
-                                    .setColor(0x0070ff.toInt())
-                                    .addField("인원:", "${Bukkit.getOnlinePlayers().size}명/ ${Bukkit.getMaxPlayers()}명",false)
+                                val title: String? = plugin.config.getString("embedTitle")
+                                val color: Int = plugin.config.getInt("embedColor")
+                                val field1: String? = plugin.config.getString("embedField1")
+                                val field2: String? = plugin.config.getString("embedField2")
 
-                                var memberStr: String = "```\n"
+                                val embed = EmbedBuilder().setTitle(title?: "**온라인 유저**")
+                                    .setColor(color)
+                                    .addField(field1?: "인원:", "${Bukkit.getOnlinePlayers().size}명/ ${Bukkit.getMaxPlayers()}명",false)
+
+                                var memberStr = "```\n"
                                 if (Bukkit.getOnlinePlayers().isNotEmpty()) {
                                     for ((i, player) in Bukkit.getOnlinePlayers().withIndex()) {
                                         memberStr += "$i 번째: ${player.name}\n"
                                     }
                                 } else {
-                                    memberStr += "사람이 없습니다\n"
+                                    memberStr += "${noMember?:"사람이 없습니다."}\n"
                                 }
                                 memberStr += "```"
 
-                                embed.addField("목록:", memberStr, false)
+                                embed.addField(field2?:  "목록:", memberStr, false)
                                 if (WildDiscord.serverAddress != null) embed.setFooter("========${WildDiscord.serverAddress ?: ""}========")
 
                                 val result: MessageEmbed = embed.build()
                                 event.channel.sendMessage(result).queue()
                             }
                         } else {
-                            Bukkit.broadcastMessage("<${ChatColor.DARK_PURPLE}${event.author.asTag}${ChatColor.RESET}> ${event.message.contentRaw}")
+                            val format: String = plugin.config.getString("messageFormat")?:"<<dark_purple><sender><reset>> <message>"
+                            val customColor: Boolean = plugin.config.getBoolean("customColor")
+                            val supportMarkdown: Boolean = plugin.config.getBoolean("customColor")
+                            Bukkit.broadcastMessage(replaceChatFormat(event, format, supportMarkdown, customColor))
                         }
                 }
             }
