fef746be4bd3439593d834475eec66167a75f93f
ProjectTL12345 <projecttl12345@gmail.com>
2021-06-05 13:48:37 +0900

    feat: add sleepCount setting

--- src/main/kotlin/xyz/netherald/wild/discord/listeners/SleepIndicator.kt
+++ src/main/kotlin/xyz/netherald/wild/discord/listeners/SleepIndicator.kt
@@ -16,7 +16,7 @@ class SleepIndicator(private val plugin: WildDiscord): Listener {
 
     @EventHandler
     fun onBedEnter(event: PlayerBedEnterEvent) {
-        sleepCount += 1
+        setSleepCount(getSleepCount() + 1)
         channel?.sendMessage("**${event.player.name}님이 수면 중 입니다! `[${sleepCount}/${plugin.server.onlinePlayers}]`**")?.queue()
         Bukkit.broadcast(Component.text("<Discord> ${ChatColor.GREEN}${event.player.name}${ChatColor.GREEN}님이 수면 중 입니다! " +
                 "${ChatColor.RESET}[${ChatColor.GOLD}${sleepCount}${ChatColor.RESET}/${ChatColor.GOLD}${plugin.server.onlinePlayers}${ChatColor.RESET}]")
@@ -25,10 +25,18 @@ class SleepIndicator(private val plugin: WildDiscord): Listener {
 
     @EventHandler
     fun onBedLeave(event: PlayerBedLeaveEvent) {
-        sleepCount -= 1
+        setSleepCount(getSleepCount() - 1)
         channel?.sendMessage("**${event.player.name}님이 침대에서 나오셨습니다! `[${sleepCount}/${plugin.server.onlinePlayers}]`**")?.queue()
         Bukkit.broadcast(Component.text("<Discord> ${ChatColor.YELLOW}${event.player.name}${ChatColor.RED}님이 침대에서 나오셨습니다! " +
                 "${ChatColor.RESET}[${ChatColor.GOLD}${sleepCount}${ChatColor.RESET}/${ChatColor.GOLD}${plugin.server.onlinePlayers}${ChatColor.RESET}]")
         )
+    }
+
+    fun getSleepCount(): Int {
+        return sleepCount
+    }
+
+    fun setSleepCount(count: Int) {
+        sleepCount = count
     }
 }
