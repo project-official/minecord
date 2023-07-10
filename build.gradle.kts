f92b8bebda5d1966837f516e633e2fcf0037cc74
myoun <aiden080605@gmail.com>
2023-07-11 02:15:02 +0900

    v4 prepare

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,22 @@
-plugins {
-    kotlin("jvm") version "1.7.20"
-}
+group = rootProject.group
+version = rootProject.version
 
-group = "dev.cube1"
-version = "3.0.0-BETA"
-
-allprojects {
-    apply(plugin = "org.jetbrains.kotlin.jvm")
-
-    java.toolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
+tasks {
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "17"
     }
 
-    repositories {
-        mavenCentral()
-    }
-}
-
-subprojects {
-    repositories {
-        mavenCentral()
-        mavenLocal()
-
-        maven("https://papermc.io/repo/repository/maven-public/")
-    }
-
-    dependencies {
-        implementation(kotlin("stdlib"))
-        implementation("io.github.monun:kommand-api:2.14.0")
-        implementation("net.dv8tion:JDA:5.0.0-alpha.22") {
-            exclude(module = "opus-java")
+    processResources {
+        filesMatching("plugin.yml") {
+            expand(project.properties)
         }
+    }
 
-        compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
-        compileOnly("org.apache.logging.log4j:log4j-core:2.19.0")
+    create<Jar>("paperJar") {
+        archiveBaseName.set(rootProject.name)
+        archiveClassifier.set("")
+        archiveVersion.set("")
+
+        from(sourceSets["main"].output)
     }
 }
---
+++ src/main/kotlin/dev/cube1/minecord/plugin/listener/ChatListener.kt
@@ -0,0 +1,4 @@
+package dev.cube1.minecord.plugin.listener
+
+class ChatListener {
+}
